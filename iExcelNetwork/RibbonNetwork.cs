3d3d374a590c45f2bade1df2b3bd526a926bc39d
Aleksejs <aleksejs.ivanovskis@gmail.com>
2024-05-12 03:35:45 +0300

    Added add-in ribbon.

---
+++ iExcelNetwork/RibbonNetwork.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Office.Tools.Ribbon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace iExcelNetwork
+{
+    public partial class RibbonNetwork
+    {
+        private void RibbonNetwork_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+    }
+}
