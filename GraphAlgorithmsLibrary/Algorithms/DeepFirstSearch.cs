3f27fe31ca29efec06d7449965895da6d8975858
Aleksejs <aleksejs.ivanovskis@gmail.com>
2024-07-26 23:29:40 +0300

    All DFS logic moved to DeepFirstSear class.

--- GraphAlgorithmsLibrary/Algorithms/DeepFirstSearch.cs
+++ GraphAlgorithmsLibrary/Algorithms/DeepFirstSearch.cs
@@ -1,46 +1,101 @@
 ﻿
+using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace GraphAlgorithmsLibrary.Algorithms
 {
     public class DeepFirstSearch
     {
         private readonly Dictionary<int, List<int>> _graph;
+        private int? maxNodeCache = null; // Cache the max node value
 
         public DeepFirstSearch(Dictionary<int, List<int>> graph)
         {
             _graph = graph;   
         }
-        
-        // Recursive DFS function to find all paths
-        public void Execute(int current, int destination, bool[] visited, List<int> path, List<List<int>> allPaths)
-        {
-            visited[current] = true;
-            path.Add(current);
 
-            // If destination is reached, add the path to allPaths
-            if (current == destination)
+        // Recursive DFS function to find all paths
+        public async Task<List<List<int>>> Execute(int src, int dst)
+        {
+            ConcurrentBag<List<int>> allPaths = new ConcurrentBag<List<int>>();
+            int maxNode = GetMaxNode();
+
+            bool[] visited = new bool[maxNode + 1];
+
+            async Task DFSAsync(int current, List<int> path, bool[] localVisited)
             {
-                allPaths.Add(new List<int>(path));
+                localVisited[current] = true;
+
+                if (current == dst)
+                {
+                    allPaths.Add(new List<int>(path));
+                }
+                else if (_graph.ContainsKey(current))
+                {
+                    var tasks = new List<Task>();
+                    foreach (var neighbor in _graph[current])
+                    {
+                        if (!localVisited[neighbor])
+                        {
+                            var newPath = new List<int>(path) { neighbor };
+                            var newVisited = (bool[])localVisited.Clone();
+                            tasks.Add(DFSAsync(neighbor, newPath, newVisited));
+                        }
+                    }
+                    await Task.WhenAll(tasks);
+                }
             }
-            else
+
+            var initialTasks = new List<(int, List<int>, bool[])>();
+            Stack<(int, List<int>)> stack = new Stack<(int, List<int>)>();
+            stack.Push((src, new List<int> { src }));
+
+            while (stack.Count > 0)
             {
-                // Recur for all the vertices adjacent to the current vertex
+                var (current, path) = stack.Pop();
                 if (_graph.ContainsKey(current))
                 {
                     foreach (var neighbor in _graph[current])
                     {
                         if (!visited[neighbor])
                         {
-                            Execute(neighbor, destination, visited, path, allPaths);
+                            var newPath = new List<int>(path) { neighbor };
+                            initialTasks.Add((neighbor, newPath, (bool[])visited.Clone()));
                         }
                     }
                 }
             }
 
-            // Remove current node from path and mark it as unvisited (backtrack)
-            path.RemoveAt(path.Count - 1);
-            visited[current] = false;
+            // Parallel execution with auto-adjusted chunks
+            var parallelOptions = new ParallelOptions
+            {
+                MaxDegreeOfParallelism = Environment.ProcessorCount
+            };
+
+            var partitioner = Partitioner.Create(initialTasks, EnumerablePartitionerOptions.NoBuffering);
+            var tasksParallel = new List<Task>();
+
+            Parallel.ForEach(partitioner, parallelOptions, initialTask =>
+            {
+                var (current, path, localVisited) = initialTask;
+                tasksParallel.Add(DFSAsync(current, path, localVisited));
+            });
+
+            await Task.WhenAll(tasksParallel);
+
+            return allPaths.ToList();
+        }
+
+        private int GetMaxNode()
+        {
+            if (maxNodeCache == null)
+            {
+                maxNodeCache = _graph.Keys.DefaultIfEmpty(-1).Max();
+            }
+            return maxNodeCache.Value;
         }
     }
 }
--- GraphAlgorithmsLibrary/GraphBase.cs
+++ GraphAlgorithmsLibrary/GraphBase.cs
@@ -1,10 +1,7 @@
 ﻿// Ignore Spelling: DFS
 
 using GraphAlgorithmsLibrary.Algorithms;
-using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace GraphAlgorithmsLibrary
@@ -12,93 +9,18 @@ namespace GraphAlgorithmsLibrary
     public abstract class GraphBase
     {
         internal readonly Dictionary<int, List<int>> graph = new Dictionary<int, List<int>>();
-        private int? maxNodeCache = null; // Cache the max node value
 
         public abstract void AddEdge(int u, int v);
 
         public async Task<List<List<int>>> DFS_FindAllPathsAsync(int src, int dst)
         {
-            ConcurrentBag<List<int>> allPaths = new ConcurrentBag<List<int>>();
-            int maxNode = GetMaxNode();
-
-            bool[] visited = new bool[maxNode + 1];
-
-            async Task DFSAsync(int current, List<int> path, bool[] localVisited)
-            {
-                localVisited[current] = true;
-
-                if (current == dst)
-                {
-                    allPaths.Add(new List<int>(path));
-                }
-                else if (graph.ContainsKey(current))
-                {
-                    var tasks = new List<Task>();
-                    foreach (var neighbor in graph[current])
-                    {
-                        if (!localVisited[neighbor])
-                        {
-                            var newPath = new List<int>(path) { neighbor };
-                            var newVisited = (bool[])localVisited.Clone();
-                            tasks.Add(DFSAsync(neighbor, newPath, newVisited));
-                        }
-                    }
-                    await Task.WhenAll(tasks);
-                }
-            }
-
-            var initialTasks = new List<(int, List<int>, bool[])>();
-            Stack<(int, List<int>)> stack = new Stack<(int, List<int>)>();
-            stack.Push((src, new List<int> { src }));
-
-            while (stack.Count > 0)
-            {
-                var (current, path) = stack.Pop();
-                if (graph.ContainsKey(current))
-                {
-                    foreach (var neighbor in graph[current])
-                    {
-                        if (!visited[neighbor])
-                        {
-                            var newPath = new List<int>(path) { neighbor };
-                            initialTasks.Add((neighbor, newPath, (bool[])visited.Clone()));
-                        }
-                    }
-                }
-            }
-
-            // Parallel execution with auto-adjusted chunks
-            var parallelOptions = new ParallelOptions
-            {
-                MaxDegreeOfParallelism = Environment.ProcessorCount
-            };
-
-            var partitioner = Partitioner.Create(initialTasks, EnumerablePartitionerOptions.NoBuffering);
-            var tasksParallel = new List<Task>();
-
-            Parallel.ForEach(partitioner, parallelOptions, initialTask =>
-            {
-                var (current, path, localVisited) = initialTask;
-                tasksParallel.Add(DFSAsync(current, path, localVisited));
-            });
-
-            await Task.WhenAll(tasksParallel);
-
-            return allPaths.ToList();
+            DeepFirstSearch deepFirstSearch = new DeepFirstSearch(graph);
+            return await deepFirstSearch.Execute(src, dst);
         }
 
         public Dictionary<int, List<int>> GetGraph()
         {
             return graph;
-        }
-
-        private int GetMaxNode()
-        {
-            if (maxNodeCache == null)
-            {
-                maxNodeCache = graph.Keys.DefaultIfEmpty(-1).Max();
-            }
-            return maxNodeCache.Value;
         }
     }   
     
