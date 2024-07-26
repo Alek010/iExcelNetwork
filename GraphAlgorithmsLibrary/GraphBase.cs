68270794b03463a58369f0c1a81a2f5c90cb9700
Aleksejs <aleksejs.ivanovskis@gmail.com>
2024-07-26 17:02:02 +0300

    Performance improvement Deep Fist Sear Find all paths.

--- GraphAlgorithmsLibrary/GraphBase.cs
+++ GraphAlgorithmsLibrary/GraphBase.cs
@@ -1,28 +1,68 @@
 ﻿// Ignore Spelling: DFS
 
 using GraphAlgorithmsLibrary.Algorithms;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace GraphAlgorithmsLibrary
 {
     public abstract class GraphBase
     {
         internal readonly Dictionary<int, List<int>> graph = new Dictionary<int, List<int>>();
+        private int? maxNodeCache = null; // Cache the max node value
 
         public abstract void AddEdge(int u, int v);
 
         public List<List<int>> DFS_FindAllPaths(int src, int dst)
         {
-            List<int> path = new List<int>();
-            List<List<int>> allPaths = new List<List<int>>();
+            ConcurrentBag<List<int>> allPaths = new ConcurrentBag<List<int>>();
             int maxNode = GetMaxNode();
 
             bool[] visited = new bool[maxNode + 1];
 
-            DeepFirstSearch deepFirstSearch = new DeepFirstSearch(graph);
-            deepFirstSearch.Execute(src, dst, visited, path, allPaths);
+            void DFS(int current, List<int> path)
+            {
+                visited[current] = true;
 
-            return allPaths;
+                if (current == dst)
+                {
+                    allPaths.Add(new List<int>(path));
+                }
+                else if (graph.ContainsKey(current))
+                {
+                    foreach (var neighbor in graph[current])
+                    {
+                        if (!visited[neighbor])
+                        {
+                            path.Add(neighbor);
+                            DFS(neighbor, path);
+                            path.RemoveAt(path.Count - 1);
+                        }
+                    }
+                }
+
+                visited[current] = false;
+            }
+
+            Stack<(int, List<int>)> stack = new Stack<(int, List<int>)>();
+            stack.Push((src, new List<int> { src }));
+
+            while (stack.Count > 0)
+            {
+                var (current, path) = stack.Pop();
+                Parallel.ForEach(graph[current], neighbor =>
+                {
+                    if (!visited[neighbor])
+                    {
+                        var newPath = new List<int>(path) { neighbor };
+                        DFS(neighbor, newPath);
+                    }
+                });
+            }
+
+            return allPaths.ToList();
         }
 
         public Dictionary<int, List<int>> GetGraph()
@@ -32,15 +72,12 @@ namespace GraphAlgorithmsLibrary
 
         private int GetMaxNode()
         {
-            int maxNode = -1;
-            foreach (var node in graph.Keys)
+            if (maxNodeCache == null)
             {
-                if (node > maxNode)
-                {
-                    maxNode = node;
-                }
+                maxNodeCache = graph.Keys.DefaultIfEmpty(-1).Max();
             }
-            return maxNode;
+            return maxNodeCache.Value;
         }
-    }
+    }   
+    
 }
