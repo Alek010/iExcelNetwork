5ade3939eeb3992a4089488d0878af9826a4ae6c
Aleksejs <aleksejs.ivanovskis@gmail.com>
2024-08-16 23:37:03 +0300

    Created INetworkData interface.

---
+++ GraphVisualizationLibrary/Interfaces/IDataRange.cs
@@ -0,0 +1,13 @@
+﻿// Ignore Spelling: json
+
+using System.Collections.Generic;
+
+namespace GraphVisualizationLibrary.Interfaces
+{
+    public interface IDataRange
+    {
+        List<string> GetFromColumnValues();
+        List<string> GetLinksCount();
+        List<string> GetToColumnValues();
+    }
+}
