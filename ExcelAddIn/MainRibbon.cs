abefd6057c7ba42b7a20827c5c6dae2415065c54
Aleksejs <aleksejs.ivanovskis@gmail.com>
2025-02-20 11:15:31 -0600

    Added Ribbon with button.

---
+++ ExcelAddIn/MainRibbon.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Office.Tools.Ribbon;
+using System.Windows.Forms;
+
+namespace ExcelAddIn
+{
+    public partial class MainRibbon
+    {
+        private void MainRibbon_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void btn_SelectRange_Click(object sender, RibbonControlEventArgs e)
+        {
+            MessageBox.Show("Click on button!");
+        }
+    }
+}
