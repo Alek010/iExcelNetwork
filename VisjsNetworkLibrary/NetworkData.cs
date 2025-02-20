0fd4faada504fc62a9949354dfd407818a980de0
Aleksejs <aleksejs.ivanovskis@gmail.com>
2025-02-20 17:01:51 -0600

    Optimized code of NetworkData class.

--- VisjsNetworkLibrary/NetworkData.cs
+++ VisjsNetworkLibrary/NetworkData.cs
@@ -18,56 +18,44 @@ namespace VisjsNetworkLibrary
 
         public List<Node> GetNodes()
         {
-            List<Node> NodesList = new List<Node>();
-
-            var nodesLabels = GetNodesLabels();
-
-            for (int i = 0; i < nodesLabels.Count; i++)
-            {
-                Node item = new Node()
-                {
-                    Id = i + 1,
-                    Label = nodesLabels[i]
-                };
-
-                NodesList.Add(item);
-            }
-
-            return NodesList;
+            return GetNodesLabels()
+                    .Select((label, index) => new Node
+                    {
+                        Id = index + 1,
+                        Label = label
+                    })
+                    .ToList();
         }
 
         public List<Edge> GetEdges()
         {
-            var EdgesList = new List<Edge>();
-            List<Node> nodes = GetNodes();
+            var nodeDict = GetNodes().ToDictionary(n => n.Label, n => n.Id);
 
-            EdgesList = _dataTable.AsEnumerable()
-            .GroupBy(row => new
-            {
-                From = row.Field<string>("from"),
-                To = row.Field<string>("to")
-            })
-            .Select(g => new Edge
-            {
-                From = nodes.Where(x => x.Label == g.Key.From).Select(x => x.Id).First(),
-                To = nodes.Where(x => x.Label == g.Key.To).Select(x => x.Id).First(),
-                Count = g.Count().ToString()
-            })
-            .ToList();
+            var edgesList = _dataTable.AsEnumerable()
+                .GroupBy(row => new
+                {
+                    From = row.Field<string>("from"),
+                    To = row.Field<string>("to")
+                })
+                .Select(g => new Edge
+                {
+                    From = nodeDict[g.Key.From],
+                    To = nodeDict[g.Key.To],
+                    Count = g.Count().ToString()
+                })
+                .ToList();
 
-            return EdgesList;
+            return edgesList;
         }
 
         private List<string> GetNodesLabels()
         {
-            List<string> nodesLabels = new List<string>();
+            var nodesLabels = _dataTable.AsEnumerable()
+                .SelectMany(row => new[] { row.Field<string>("from"), row.Field<string>("to") })
+                .Distinct()
+                .ToList();
 
-            nodesLabels.AddRange(_dataTable.AsEnumerable().Select(x => x.Field<string>("from")));
-            nodesLabels.AddRange(_dataTable.AsEnumerable().Select(x => x.Field<string>("to")));
-
-            List<string> uniqueNodesLabels = nodesLabels.Distinct().ToList();
-
-            return uniqueNodesLabels;
+            return nodesLabels;
         }
     }
 }
