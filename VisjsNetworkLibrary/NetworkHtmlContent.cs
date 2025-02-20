77468a19d2e9d2be7a19a6594bf2f5d9c4861ac0
Aleksejs <aleksejs.ivanovskis@gmail.com>
2025-02-20 11:40:34 -0600

    Created NetworkHtmlContent class. Generates HTML file from template and substituting values.

---
+++ VisjsNetworkLibrary/NetworkHtmlContent.cs
@@ -0,0 +1,47 @@
+﻿// Ignore Spelling: Visjs
+
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace VisjsNetworkLibrary
+{
+    public class NetworkHtmlContent
+    {
+        private string _nodesJson { get; set; }
+        private string _edgesJson { get; set; }
+
+        public NetworkHtmlContent()
+        {
+
+        }
+
+        public string GenerateFileContent()
+        {
+            string VisJsScript = GetEmbeddedResource("VisjsNetworkLibrary.Resources.vis-network.min.js");
+
+            string htmlTemplate = GetEmbeddedResource("VisjsNetworkLibrary.Resources.VisJsNetworkTemplate.html");
+
+            return htmlTemplate
+                .Replace("{{VisJsScript}}", VisJsScript)
+                .Replace("{{nodesJson}}", _nodesJson)
+                .Replace("{{edgesJson}}", _edgesJson)
+                .Replace("selectedEdgesDirection", "to");
+        }
+
+        public string GetFilePath()
+        {
+            return Path.Combine(Path.GetTempPath(), "VisjsNetwork") + ".html";
+        }
+
+        private string GetEmbeddedResource(string resourceName)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
