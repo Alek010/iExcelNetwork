5042a47cd6d2fd6cea2745c53766b75f424178bd
Aleksejs <aleksejs.ivanovskis@gmail.com>
2025-02-27 04:26:06 -0600

    Created tests to NetworkDataWithCount class. Tested only GetEdges() as GetNodes are same as parent class.

---
+++ VisjsNetworkLibraryTests/NetworkDataWithCountTests.cs
@@ -0,0 +1,72 @@
+﻿// Ignore Spelling: Visjs
+
+using System.Data;
+using VisjsNetworkLibrary.Models;
+using VisjsNetworkLibrary;
+using VisjsNetworkLibrary.Exceptions;
+
+namespace VisjsNetworkLibraryTests
+{
+    public class NetworkDataWithCountTests
+    {
+        [Fact]
+        public void GetEdges_WithMultipleUniqueRowsTable_ExtractsCorrectEdges()
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("from", typeof(string));
+            dt.Columns.Add("to", typeof(string));
+            dt.Columns.Add("count", typeof(string));
+
+            dt.Rows.Add("A", "B", "1");
+            dt.Rows.Add("C", "B", "2");
+
+            NetworkDataWithCount networkData = new NetworkDataWithCount(dt);
+
+            List<Edge> edges = networkData.GetEdges();
+
+            Assert.Equal(2, edges.Count);
+
+            Assert.Equivalent(new Edge() { From = 1, To = 2, Count = "1" }, edges[0]);
+            Assert.Equivalent(new Edge() { From = 3, To = 2, Count = "2" }, edges[1]);
+        }
+
+        [Fact]
+        public void GetEdges_WithDuplicateRowsTable_ExtractsCorrectEdges()
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("from", typeof(string));
+            dt.Columns.Add("to", typeof(string));
+            dt.Columns.Add("count", typeof(string));
+
+            dt.Rows.Add("C", "B", "5");
+            dt.Rows.Add("A", "B", "1");
+            dt.Rows.Add("C", "B", "5");
+
+
+            NetworkDataWithCount networkData = new NetworkDataWithCount(dt);
+
+            List<Edge> edges = networkData.GetEdges();
+
+            Assert.Equal(2, edges.Count);
+
+            Assert.Equivalent(new Edge() { From = 1, To = 2, Count = "10" }, edges[0]);
+            Assert.Equivalent(new Edge() { From = 3, To = 2, Count = "1" }, edges[1]);
+        }
+
+        [Fact]
+        public void GetEdges_WithCountColumnValuesNonInteger_ThrowsDataTableStructureException()
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("from", typeof(string));
+            dt.Columns.Add("to", typeof(string));
+            dt.Columns.Add("count", typeof(string));
+
+            dt.Rows.Add("C", "B", "x");
+
+            NetworkDataWithCount networkData = new NetworkDataWithCount(dt);
+
+            var exception = Assert.Throws<DataTableStructureException>(() => networkData.GetEdges());
+            Assert.Equal("Not all count column values are integers.", exception.Message);
+        }
+    }
+}
