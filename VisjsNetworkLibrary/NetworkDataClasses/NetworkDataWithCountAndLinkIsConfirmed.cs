e3bc07694dc72fd6cbff77df2bd94fed6f74b54e
Aleksejs <aleksejs.ivanovskis@gmail.com>
2025-03-20 09:25:30 -0600

    Created NetworkDataithCountAndLinkIsConfirmed class

---
+++ VisjsNetworkLibrary/NetworkDataClasses/NetworkDataWithCountAndLinkIsConfirmed.cs
@@ -0,0 +1,14 @@
+﻿// Ignore Spelling: Visjs
+
+using System.Data;
+using VisjsNetworkLibrary.Interfaces;
+
+namespace VisjsNetworkLibrary.NetworkDataClasses
+{
+    public class NetworkDataWithCountAndLinkIsConfirmed : NetworkData, INetworkData
+    {
+        public NetworkDataWithCountAndLinkIsConfirmed(DataTable dataTable) : base(dataTable)
+        {
+        }
+    }
+}
