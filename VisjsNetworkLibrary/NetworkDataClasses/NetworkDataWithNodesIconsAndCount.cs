5ed43ec90b164c4d41dabdc6b3ed1659a44693c2
Aleksejs <aleksejs.ivanovskis@gmail.com>
2025-03-21 15:28:27 -0600

    Created NetworkDataWithNodesIconsAndCount class.

---
+++ VisjsNetworkLibrary/NetworkDataClasses/NetworkDataWithNodesIconsAndCount.cs
@@ -0,0 +1,14 @@
+﻿// Ignore Spelling: Visjs
+
+using System.Data;
+using VisjsNetworkLibrary.Interfaces;
+
+namespace VisjsNetworkLibrary.NetworkDataClasses
+{
+    public class NetworkDataWithNodesIconsAndCount : NetworkData, INetworkData
+    {
+        public NetworkDataWithNodesIconsAndCount(DataTable dataTable) : base(dataTable)
+        {
+        }
+    }
+}
