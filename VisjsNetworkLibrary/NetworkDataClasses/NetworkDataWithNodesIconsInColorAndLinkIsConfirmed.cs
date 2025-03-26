4227643d41b07f2d45d24b841e159299a70c19cb
Aleksejs <aleksejs.ivanovskis@gmail.com>
2025-03-26 14:59:31 -0600

    Created class NetworkDataWithNodesIconsInColorAndLinkIsConfirmed

---
+++ VisjsNetworkLibrary/NetworkDataClasses/NetworkDataWithNodesIconsInColorAndLinkIsConfirmed.cs
@@ -0,0 +1,14 @@
+﻿// Ignore Spelling: Visjs
+
+using System.Data;
+using VisjsNetworkLibrary.Interfaces;
+
+namespace VisjsNetworkLibrary.NetworkDataClasses
+{
+    public class NetworkDataWithNodesIconsInColorAndLinkIsConfirmed : NetworkData, INetworkData
+    {
+        public NetworkDataWithNodesIconsInColorAndLinkIsConfirmed(DataTable dataTable) : base(dataTable)
+        {
+        }
+    }
+}
