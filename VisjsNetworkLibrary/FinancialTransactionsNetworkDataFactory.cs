823949959c2782031b4284f11248906c79cfcc92
Aleksejs <aleksejs.ivanovskis@gmail.com>
2025-06-23 18:56:11 -0600

    Implemented FinancialNetworkDataWithNodesIconsInColorAndCount class

--- VisjsNetworkLibrary/FinancialTransactionsNetworkDataFactory.cs
+++ VisjsNetworkLibrary/FinancialTransactionsNetworkDataFactory.cs
@@ -4,6 +4,7 @@ using System.Data;
 using VisjsNetworkLibrary.Exceptions;
 using VisjsNetworkLibrary.FinancialTransactionsNetworkData;
 using VisjsNetworkLibrary.Interfaces;
+using VisjsNetworkLibrary.NetworkDataClasses;
 
 namespace VisjsNetworkLibrary
 {
@@ -22,6 +23,10 @@ namespace VisjsNetworkLibrary
             else if (_columnCount == 5 && _columnNames.Contains("from") && _columnNames.Contains("fromicon") && _columnNames.Contains("to") && _columnNames.Contains("toicon") && _columnNames.Contains("count"))
             {
                 return new FinancialNetworkDataWithNodesIconsAndCount(_dataTable);
+            }
+            else if (_columnCount == 7 && _columnNames.Contains("from") && _columnNames.Contains("fromicon") && _columnNames.Contains("to") && _columnNames.Contains("toicon") && _columnNames.Contains("fromcolor") && _columnNames.Contains("tocolor") && _columnNames.Contains("count"))
+            {
+                return new FinancialNetworkDataWithNodesIconsInColorAndCount(_dataTable);
             }
             else
             {
